529217117fbff69aec82b110987261fb970a8b57
Itsbabin <itsbabin2004@gmail.com>
2024-09-20 05:23:04 +0530

    start of project

--- src/app.js
+++ src/app.js
@@ -4,11 +4,18 @@ import cors from 'cors'
 const app = express();
 
 app.use(cors({
-    origin : "*",
+    origin : process.env.CORS_ORIGIN,
     optionsSuccessStatus: 200,
     credentials : true
 }))
 
+app.use(express.json({
+    limit: "20kb",
+}))
+app.use(express.urlencoded({
+    extended: true,
+    limit: "20kb"
+}))
 
 
 
---
+++ src/const.js
@@ -0,0 +1,3 @@
+const DB_URL = "";
+
+export {DB_URL} ;
