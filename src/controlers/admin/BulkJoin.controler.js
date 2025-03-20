68547eac702fc7d1ff2aabe77f0d63087d8819e0
Itsbabin <itsbabin2004@gmail.com>
2025-03-21 01:51:15 +0530

    motamuti

--- src/controlers/admin/BulkJoin.controler.js
+++ src/controlers/admin/BulkJoin.controler.js
@@ -9,9 +9,10 @@ export default async function BulkJoinControler(req, res) {
         
     await agents.forEach(agent => {
         
-        agent.kyc = 0,
+        agent.kyc = 0,``
         agent.bv = 0,
         agent.juniors = [];
+        agent.profile_pic_URL = "https://res.cloudinary.com/dxg0zpyu4/image/upload/f_auto,q_auto/xzjbpzsgoddt54v00hxs"
 
         for(let index = 0; index < agents.length; index++) {
                 if (agent.userid == agents[index].introducer.id) {
