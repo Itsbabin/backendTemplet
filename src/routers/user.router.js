6e5d27cf0809b6e603a92665d443f3c07f0094b8
Itsbabin <itsbabin2004@gmail.com>
2025-03-25 04:10:19 +0530

    small bug fix

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -19,9 +19,5 @@ router.route('/search').post(SearchUserControler);
 router.route('/update/password').post(verifyJwtUser,UpdatePasswordControler);
 router.route('/update/address').post(verifyJwtUser,UpdateAddressControler);
 router.route('/upload/ProfilePic').post(verifyJwtUser,upload.single('profilePic'),UploadProfilePic);
-router.route('/upload/adhaar').post(verifyJwtUser,upload.single('profilePic'),UploadAdhaar);
-router.route('/upload/pan').post(verifyJwtUser,upload.single('profilePic'),UploadPan);
-router.route('/upload/kyc_profile_pic_URL').post(verifyJwtUser,upload.single('profilePic'),UploadKycPic);
-router.route('/upload/signature_pic_URL').post(verifyJwtUser,upload.single('profilePic'),UploadSignaturePic);
 
 export default router;
